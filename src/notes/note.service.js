05152e1ae5688396a7d0c7c9457cf7390f751e43
yamodah <yassinedahlek@gmail.com>
2021-12-10 16:22:17 -0500

    built out service file with basic structure and exports

---
+++ src/notes/note.service.js
@@ -0,0 +1,24 @@
+const knex = require("../db/connection")
+
+const list=()=>{
+
+}
+const read=(id)=>{
+
+}
+const update=(updatedNote)=>{
+
+}
+const create=(newNote)=>{
+
+}
+const destroy=(id)=>{
+
+}
+module.exports={
+    list,
+    read,
+    update,
+    create,
+    destroy,
+}
