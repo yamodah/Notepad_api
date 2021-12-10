2b5102ef7a641f454599d7048f8b8bbf36349e14
yamodah <yassinedahlek@gmail.com>
2021-12-10 16:08:45 -0500

    created and filled out migration file

--- src/db/migrations/20211210204637_createNotesTable.js
+++ src/db/migrations/20211210204637_createNotesTable.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("notes",(table)=>{
+      table.increments("id").primary()
+      table.string("title")
+      table.text("content")
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("notes")
 };
