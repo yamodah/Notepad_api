c5ef5be95b35b75df88bc1a2550e7623476dfb67
yamodah <yassinedahlek@gmail.com>
2021-12-10 16:27:53 -0500

    basic crud built out for service file

--- src/notes/note.service.js
+++ src/notes/note.service.js
@@ -1,19 +1,29 @@
 const knex = require("../db/connection")
 
 const list=()=>{
-
+    return knex("notes")
+        .select("*")
 }
 const read=(id)=>{
-
+    return knex("notes")
+        .select("*")
+        .where({id})
+        .first()
 }
 const update=(updatedNote)=>{
-
+    return knex("notes")
+        .select("*")
+        .where({id:updatedNote.id})
+        .update(updatedNote)
 }
 const create=(newNote)=>{
-
+    return knex("notes")
+        .insert(newNote)
 }
 const destroy=(id)=>{
-
+    return knex("notes")
+    .where({id})
+    .del()
 }
 module.exports={
     list,
