9a0f8050752bbf0757b3a348834eff8d170fc020
yamodah <yassinedahlek@gmail.com>
2021-12-10 15:50:50 -0500

    added migration and modified knexfile and added dotenv

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,33 @@
-// Update with your config settings.
+const path = require("path");
+
+require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+} = process.env;
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+}
